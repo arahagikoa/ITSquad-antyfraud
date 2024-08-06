185bbf85492c6dc5cbd09e2167c7dfea834baa07
arahagikoa <krzeminski.kamil@interia.eu>
2024-08-06 12:19:02 +0200

    updated assets, policy and relations

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import csv
 
 # Path to your CSV file
-csv_file_path = r'E:\Projekty\for_work\db-data\ITSquad-antyfraud\excel\final.csv'
+csv_file_path = r'E:\Projekty\for_work\db-data\ITSquad-antyfraud\excel\relation-incident-asset.csv'
 
 # Read data from CSV
 updates = []
@@ -9,22 +9,16 @@ with open(csv_file_path, mode='r', newline='') as file:
     reader = csv.DictReader(file, delimiter=';')  # Adjust delimiter if necessary
     for row in reader:
         updates.append((
-            row['incident_id'].strip(),  # Ensure no leading/trailing spaces
-            row['incident_type_id'].strip(),  # Ensure no leading/trailing spaces
-            row['location_id'].strip()  # Ensure no leading/trailing spaces
+            row['victim'].strip()
         ))
 
 # Template for the SQL update statement
 sql_template = """
-UPDATE public.incident
+UPDATE public.relation_incident_asset
 SET 
-    incident_type_id = CASE id 
-        {incydent_status_id_cases}
-    END,
-    location_id = CASE id 
-        {location_id_cases}
-    END
-WHERE id IN ({incident_ids});
+    system_asset_category_id = 'ca262f44-8af6-4074-aeae-6b68a2a4bb58'
+
+WHERE system_asset_category_id IN ({incident_ids});
 """
 
 # Helper function to create CASE statements
@@ -32,17 +26,15 @@ def create_case_statements(updates, column_index):
     return " ".join([f"WHEN '{record[0]}' THEN '{record[column_index]}'::uuid" for record in updates])
 
 # Creating the SQL update statement
-incydent_type_id_cases = create_case_statements(updates, 1)
-location_id_cases = create_case_statements(updates, 2)
-incident_ids = ", ".join([f"'{record[0]}'::uuid" for record in updates])
+#incydent_type_id_cases = create_case_statements(updates, 1)
+#location_id_cases = create_case_statements(updates, 2)
+incident_ids = ", ".join([f"'{record}'::uuid" for record in updates])
 
 # Format the SQL template with the case statements and incident ids
 sql_update_statement = sql_template.format(
-    incydent_status_id_cases=incydent_type_id_cases,
-    location_id_cases=location_id_cases,
     incident_ids=incident_ids
 )
 # Print the SQL update statement
-with open('./update_query.txt', 'w', encoding='utf-8') as f:
+with open('./update_query_assets_relation.txt', 'w', encoding='utf-8') as f:
     f.write(sql_update_statement)
 
